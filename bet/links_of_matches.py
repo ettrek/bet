ab14abf7f3d8b021d07913f7f4cdc81df1669945
REKSAR DSV <reksarka@gmail.com>
2018-01-19 16:27:22 +0200

    Adds bet.options module.

--- bet/links_of_matches.py
+++ bet/links_of_matches.py
@@ -1,5 +1,18 @@
-class Link:
+"""
+Here we parse HTML tree, that represents the page with annotation of football
+matches. The URL of it page is stored in `bet.options.MATCHES_ANNOTATION_URL`.
 
+We need to get the list of all links to pages with football matches details.
+
+This module have one public method `read()` that returns a `list` of `Link`
+instances.
+"""
+
+
+class Link:
+    """
+    Represents a link to a football match details page.
+    """
     def __init__(self, url="", title=""):
         self.url = url
         self.title = title
---
+++ bet/options.py
@@ -0,0 +1,1 @@
+MATCHES_ANNOTATION_URL = "http://www.oddsportal.com/matches/soccer"
